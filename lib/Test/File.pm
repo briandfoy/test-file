794f344a670cc123b02e53d03e6bbdc75dffc8e1
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:05:58 -0500

    Update copyright statement

--- lib/Test/File.pm
+++ lib/Test/File.pm
@@ -1686,7 +1686,7 @@ stuff.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2002-2014 brian d foy.  All rights reserved.
+Copyright © 2002-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
