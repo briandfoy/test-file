183a5096c97098d501e664719e30f249140eac19
brian d foy <briandfoy@pobox.com>
2025-01-03 01:49:28 -0500

    update email and copyright year

--- lib/Test/File.pm
+++ lib/Test/File.pm
@@ -1803,7 +1803,7 @@ This module is in Github:
 
 =head1 AUTHOR
 
-brian d foy, C<< <bdfoy@cpan.org> >>
+brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 CREDITS
 
@@ -1831,7 +1831,7 @@ new since Win32 0.55.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2002-2023, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2002-2025, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the Artistic License 2.0
